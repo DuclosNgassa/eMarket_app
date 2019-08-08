35c6442a1864230d6b11801afc2cdcf2f0e97607
Duclos Ngassa <armel-duclos.ndanji-ngassa@adesso.de>
2019-08-08 05:46:33 +0200

    Categories items added: work in progress

--- lib/pages/categorie_page.dart
+++ lib/pages/categorie_page.dart
@@ -19,31 +19,31 @@ class _CategoriePageState extends State<CategoriePage> {
       ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          return _buildTiles(categories[index]);
+          return _buildCategories(categories[index]);
         },
         itemCount: categories.length,
       ),
     );
   }
 
-  Widget _buildTiles(Categorie t) {
-    if (t.children.isEmpty)
+  Widget _buildCategories(Categorie categorie) {
+    if (categorie.children.isEmpty)
       return new ListTile(
           dense: true,
           enabled: true,
           isThreeLine: false,
           onLongPress: () => print("long press"),
-          onTap: () => submitCategorie(t),
-          subtitle: new Text("Subtitle"),
-          leading: new Text("Leading"),
+          onTap: () => submitCategorie(categorie),
+          //subtitle: new Text("Subtitle"),
+          //leading: new Text("Leading"),
           selected: true,
-          trailing: new Text("trailing"),
-          title: new Text(t.title));
+          //trailing: new Text("trailing"),
+          title: new Text(categorie.title));
 
     return new ExpansionTile(
-
-      title: new Text(t.title),
-      children: t.children.map(_buildTiles).toList(),
+      key: PageStorageKey<Categorie>(categorie),
+      title: new Text(categorie.title),
+      children: categorie.children.map(_buildCategories).toList(),
     );
   }
 
@@ -51,40 +51,6 @@ class _CategoriePageState extends State<CategoriePage> {
     Navigator.of(context).pop(t.title);
   }
 }
-
-/*
-class CategorieInTiles extends StatelessWidget{
-  final MyTile myTile;
-
-  CategorieInTiles(this.myTile);
-  @override
-  Widget build(BuildContext context) {
-
-    return _buildTiles(myTile);
-  }
-
-  Widget _buildTiles(MyTile t){
-    if (t.children.isEmpty)
-      return new ListTile(
-          dense: true,
-          enabled: true,
-          isThreeLine: false,
-          onLongPress: () => print("long press"),
-          onTap: () => print(t.title),
-          subtitle: new Text("Subtitle"),
-          leading: new Text("Leading"),
-          selected: true,
-          trailing: new Text("trailing"),
-          title: new Text(t.title));
-
-    return new ExpansionTile(
-      key: new PageStorageKey<int>(3),
-      title: new Text(t.title),
-      children: t.children.map(_buildTiles).toList(),
-    );
-  }
-}
-*/
 
 class Categorie {
   String title;
@@ -94,21 +60,6 @@ class Categorie {
 }
 
 List<Categorie> categories = <Categorie>[
-  new Categorie(
-    'Animals',
-    <Categorie>[
-      new Categorie(
-        'Dogs',
-        <Categorie>[
-          new Categorie('Coton de Tulear'),
-          new Categorie('German Shepherd'),
-          new Categorie('Poodle'),
-        ],
-      ),
-      new Categorie('Cats'),
-      new Categorie('Birds'),
-    ],
-  ),
   new Categorie(
     'Autos, Rad & Boot',
     <Categorie>[
@@ -123,8 +74,11 @@ List<Categorie> categories = <Categorie>[
           new Categorie('Desperate'),
           new Categorie('A3')
         ]),
-        new Categorie('BMW',
-            <Categorie>[new Categorie('X1'), new Categorie('X2'), new Categorie('X5')]),
+        new Categorie('BMW', <Categorie>[
+          new Categorie('X1'),
+          new Categorie('X2'),
+          new Categorie('X5')
+        ]),
         new Categorie('Mercedes', <Categorie>[
           new Categorie('C'),
           new Categorie('Benz'),
@@ -262,14 +216,14 @@ List<Categorie> categories = <Categorie>[
           new Categorie('Hobby'),
           new Categorie('Carado')
         ]),
-       new Categorie('Wohnwagen', <Categorie>[
+        new Categorie('Wohnwagen', <Categorie>[
           new Categorie('Adria'),
           new Categorie('Fiat'),
           new Categorie('Ford'),
           new Categorie('Hobby'),
           new Categorie('Carado')
         ]),
-       new Categorie('Weitere Wohnwagen & -mobile', <Categorie>[
+        new Categorie('Weitere Wohnwagen & -mobile', <Categorie>[
           new Categorie('Adria'),
           new Categorie('Fiat'),
           new Categorie('Ford'),
@@ -280,28 +234,113 @@ List<Categorie> categories = <Categorie>[
       new Categorie('Weiteres Auto, Rad & Boot'),
     ],
   ),
-  new Categorie(
-    'Phones',
-    <Categorie>[
-      new Categorie('Google'),
+  new Categorie('Dienstleistungen', <Categorie>[
+    new Categorie('Altenpflege'),
+    new Categorie('Auto, Rad, Boot'),
+    new Categorie('Babysitter & Kinderbetreuung'),
+    new Categorie('Elektronik'),
+    new Categorie('Haus & Garten'),
+    new Categorie('Künstler & Musiker'),
+    new Categorie('Reise & Event'),
+    new Categorie('Tierbetreuung & Training'),
+    new Categorie('Umzug & Transport'),
+    new Categorie('Weitere Dienstleistungen')
+  ]),
+  new Categorie('Eintrittskarten & Tickets', <Categorie>[
+    new Categorie('Bahn & ÖPVN'),
+    new Categorie('Comedy & Kabarett'),
+    new Categorie('Gutscheine'),
+    new Categorie('Kinder'),
+    new Categorie('Konzerte'),
+    new Categorie('Sport'),
+    new Categorie('Theater & Musical'),
+    new Categorie('Weitere Eintrittskarten & Tickets')
+  ]),
+  new Categorie('Elektronik', <Categorie>[
+    new Categorie('Audio & Hifi', <Categorie>[
+      new Categorie('CD Player'),
+      new Categorie('Lautsprächer & Kopfhörer'),
+      new Categorie('MP3 Player'),
+      new Categorie('Radio & Receiver'),
+      new Categorie('Stereoanlagen'),
+      new Categorie('Weiteres Audio & Hifi'),
+    ]),
+    new Categorie('Dienstleistungen Elektronik'),
+    new Categorie('Foto'),
+    new Categorie('Handy & Telefon', <Categorie>[
+      new Categorie('Apple'),
+      new Categorie('HTC'),
+      new Categorie('LG'),
+      new Categorie('Motorola'),
+      new Categorie('Nokia'),
+      new Categorie('Huawai'),
       new Categorie('Samsung'),
-      new Categorie(
-        'OnePlus',
-        <Categorie>[
-          new Categorie('1'),
-          new Categorie('2'),
-          new Categorie('3'),
-          new Categorie('4'),
-          new Categorie('5'),
-          new Categorie('6'),
-          new Categorie('7'),
-          new Categorie('8'),
-          new Categorie('9'),
-          new Categorie('10'),
-          new Categorie('11'),
-          new Categorie('12'),
-        ],
-      ),
-    ],
-  ),
+      new Categorie('Techno'),
+      new Categorie('Sony'),
+      new Categorie('Siemens'),
+      new Categorie('Sony'),
+      new Categorie('Faxgerät'),
+      new Categorie('Telefone'),
+      new Categorie('Weitere Handys & Telefone')
+    ]),
+    new Categorie('Haushaltsgeräte', <Categorie>[
+      new Categorie('Haushaltskleingeräte'),
+      new Categorie('Herde & Backöfen'),
+      new Categorie('Kaffe & Espressomaschinen'),
+      new Categorie('Kühlschränke & Gefriergeräte'),
+      new Categorie('Spühlmaschinen'),
+      new Categorie('Staubsauger'),
+      new Categorie('Waschmaschinen & Trockner'),
+      new Categorie('Weitere Haushaltsgeräte')
+    ]),
+    new Categorie('Konsole', <Categorie>[
+      new Categorie('Pocket Konsolen'),
+      new Categorie('PlayStation'),
+      new Categorie('Xbox'),
+      new Categorie('Wii'),
+      new Categorie('Gameboy'),
+      new Categorie('Weitere Konsolen')
+    ]),
+    new Categorie('Notebooks'),
+    new Categorie('PCs'),
+    new Categorie('PC-Zubehör & Software', <Categorie>[
+      new Categorie('Drucker & Scanner'),
+      new Categorie('festplatten & Laufwerke'),
+      new Categorie('Gehäuser'),
+      new Categorie('Grafikkarten'),
+      new Categorie('Kabel & Adapter'),
+      new Categorie('Mainboards'),
+      new Categorie('Monitore'),
+      new Categorie('Multimedia'),
+      new Categorie('Netzwerk & Modem'),
+      new Categorie('Prozessoren / CPUs'),
+      new Categorie('Speicher'),
+      new Categorie('Software'),
+      new Categorie('Tastatur & Maus'),
+      new Categorie('Weitere PC-Zubehör'),
+    ]),
+    new Categorie('Tablets & Reader', <Categorie>[
+      new Categorie('iPad'),
+      new Categorie('Kindle'),
+      new Categorie('Samsung Tablets'),
+      new Categorie('Huawai Tablets'),
+      new Categorie('Weitere Tablets & Reader')
+    ]),
+    new Categorie('TV & Video', <Categorie>[
+      new Categorie('DVD-Player & Recorder'),
+      new Categorie('Fernseher'),
+      new Categorie('TV-Receiver'),
+      new Categorie('Weitere TV & Video')
+    ]),
+    new Categorie('Videospiele', <Categorie>[
+      new Categorie('DS(i)- & PSP Spiele'),
+      new Categorie('Nintendo Spiele'),
+      new Categorie('PlayStation Spiele'),
+      new Categorie('Xbox Spiele'),
+      new Categorie('Wii Spiele'),
+      new Categorie('PC Spiele'),
+      new Categorie('Weitere Videospiele')
+    ]),
+    new Categorie('Weitere Elektronik')
+  ]),
 ];
